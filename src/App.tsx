b0a3a4717ffe10c6bb9221d09469da18d1c9e7f3
Alesh <floydspink49@gmail.com>
2024-05-15 12:44:04 +0545

    react

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ const App = () => {
   }
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <Header/>
       <Display
         FirstName={'Alesh'}
