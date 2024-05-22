6e7d6ecfd3aa0138d9d8e3b4a94485a7eef24355
Alesh <floydspink49@gmail.com>
2024-05-22 12:35:34 +0545

    product details fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./Pages/Home/Home";
 import Contact from "./Pages/Contact/Contact";
 import Pricing from "./Pages/Pricing/Pricing";
 import ProductPage from "./Pages/Products/Products";
+import SingleProduct from "./Pages/Products/product-detail";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Products" element={<ProductPage/>} />
+        <Route path="/Products/:id" element={<SingleProduct/>} />
         <Route path="/Pricing" element={<Pricing/>} />
         <Route path="/Contact" element={<Contact/>} />
         
---
+++ src/Pages/Products/product-detail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductDetail from "../../component/product/product-details";
+import { useParams } from "react-router-dom";
+
+const SingleProduct = () => {
+  const params = useParams();
+  const id = params.id;
+  console.log(params);
+  return <div>{id && <ProductDetail id={Number(id)} />}</div>;
+};
+
+export default SingleProduct;
